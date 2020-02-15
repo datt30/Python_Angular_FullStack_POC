4d52ae45a81ff1c203cd4549391ac3640b94c58c
datt30 <davidtamayo30@gmail.com>
2020-02-15 12:10:14 -0500

    added script for create our tables
    added script for create our tables

---
+++ db_scripts/tables.sql
@@ -0,0 +1,53 @@
+
+
+CREATE TABLE Client (
+    client_id int NOT NULL IDENTITY PRIMARY KEY,
+    identity_number int NOT NULL,
+    age int,
+    client_name varchar(25),
+    client_surname varchar(25)
+);
+
+CREATE TABLE Product (
+    product_id int NOT NULL IDENTITY PRIMARY KEY,
+    product_name varchar(25),
+    price money,
+    detail varchar(255)
+);
+
+CREATE TABLE ExternalProvider (
+    external_provider_id int NOT NULL IDENTITY PRIMARY KEY,
+    external_provider_name varchar(25),
+    phone int,
+    detail varchar(255),
+);
+
+CREATE TABLE ExternalProviderXProduct (
+    external_provider_id int NOT NULL IDENTITY PRIMARY KEY,
+    product_id int NOT NULL,
+    price money,
+    detail varchar(255),
+    product_id int FOREIGN KEY REFERENCES Product(product_id),
+    external_provider_id int FOREIGN KEY REFERENCES ExternalProvider(external_provider_id)
+);
+
+CREATE TABLE Bill (
+    bill_id int NOT NULL IDENTITY PRIMARY KEY,
+    bill_date Date,
+    taxes money,
+    sub_total money,
+    total money,
+    client_id int FOREIGN KEY REFERENCES Client(client_id)
+);
+
+
+CREATE TABLE BillXProduct (
+    bill_x_product_id int NOT NULL IDENTITY PRIMARY KEY,
+    quantity int,
+    bill_id int FOREIGN KEY REFERENCES Bill(bill_id),
+    product_id int FOREIGN KEY REFERENCES Product(product_id),
+);
+
+
+
+
